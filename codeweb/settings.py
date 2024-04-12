a54e6add6df3648ad79d262ec4e666f40e5d7011
cuong-sadboi <cuongdelldz@gmail.com>
2024-04-13 00:36:37 +0700

    django authemtication

--- codeweb/settings.py
+++ codeweb/settings.py
@@ -140,3 +140,11 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+
+#SMTP configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'cuongdelldz@gmail.com'
+EMAIL_HOST_PASSWORD = 'fhsc afci bxzx enia'
+EMAIL_USE_TLS = True
